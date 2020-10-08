b1c609074ef1f280509b9dedf0cd6d1bc6cba74f
Artem <tommerg09@gmail.com>
2020-10-08 22:35:23 +0300

    init: start project

---
+++ portfolio/src/components/app/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from '../header/header';
+import './app.css';
+
+export default class App extends Component {
+  state = {
+    portfolio: true
+  }
+
+  render() {
+    return (
+      <div className="portfolio">
+
+      </div>
+    )
+  }  
+}
+
